18d78f333dc9c5759f835a6ea549298241943b3f
allan <allan.hal@gmail.com>
2022-10-11 11:09:04 -0300

    buscar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav/index.js";
 import Produtos from "./components/Produtos/index.js";
 import React, { useState } from "react";
+import Buscar from "./components/Nav/Buscar/index.js";
 
 export const Cachorro = React.createContext();
 
@@ -8,13 +9,12 @@ export default function App() {
   const [carrinho, setCarrinho] = useState([]);
 
   return (
-    <Cachorro.Provider value={{ carrinho, setCarrinho }}>
-      <>
-        <Nav />
-        <Produtos />
-      </>
-    </Cachorro.Provider>
-  )
+      <Buscar />
+    // <Cachorro.Provider value={{ carrinho, setCarrinho }}>
+    //   <>
+    //     <Nav />
+    //     <Produtos />
+    //   </>
+    // </Cachorro.Provider>
+  );
 }
-
-
--- src/components/Nav/Buscar/index.js
+++ src/components/Nav/Buscar/index.js
@@ -1,8 +1,24 @@
-import React from "react";
-import './index.css'
+import React, { useState } from "react";
+import "./index.css";
+import { Search } from "@mui/icons-material";
 
 export default function Buscar() {
-    return (
-        <span>Buscar</span>
-    )
+  const [conteudo, setConteudo] = useState("");
+
+  let classes = "input";
+  if (conteudo) {
+    classes += " input-expanded";
+  }
+
+  function handleOnChange(event) {
+    setConteudo(event.target.value);
+  }
+
+  return (
+    <div className="wrapper">
+      <Search className="icon" />
+      <input className={classes} placeholder="Buscar" value={conteudo} onChange={(event) => { handleOnChange(event); }}
+      />
+    </div>
+  );
 }
