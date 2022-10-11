28dfe819cc8bda2a9a684490a39716bfcab63e8f
angelolustosa <alessandrofeitoza01@gmail.com>
2022-10-11 10:12:28 -0300

    buscar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,14 @@
 import Carrinho from "../Carrinho"
+import Buscar from "./Buscar"
 
-export default function Nav(){
+export default function Nav() {
+    
     return (
-        <div>
+        <div style={{
+            color: 'red',
+        }}>
             <h1>Nav</h1>
+            <Buscar />
             <Carrinho />
         </div>
     )
